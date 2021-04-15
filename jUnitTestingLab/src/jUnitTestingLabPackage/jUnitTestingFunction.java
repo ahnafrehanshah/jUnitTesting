8fb5ffc7151f595b6b4d11879db64539f2a8f8c0
ahnafrehanshah <ahanafrehanshah@gmail.com>
2021-04-16 01:01:32 +0530

    commitit

---
+++ jUnitTestingLab/src/jUnitTestingLabPackage/jUnitTestingFunction.java
@@ -0,0 +1,10 @@
+package jUnitTestingLabPackage;
+
+public class jUnitTestingFunction {
+	public int addNumbers(int num1, int num2) {
+		return num1+num2;
+	}
+	public String addString(String s1, String s2) {
+		return s1+s2;
+	}
+}
